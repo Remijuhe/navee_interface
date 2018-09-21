ccf309cd0d05dea0b85d0b3766ee9cb3021bd894
Remi Juhe <remi.juhe@edu.escpeurope.eu>
2018-09-21 12:09:00 +0200

    post model created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+p "Destroy all posts"
+Post.destroy_all
+p "Generating all posts"
+csv_options = { col_sep: '|', quote_char: '"', headers: :first_row }
+filepath    = 'studapart_no_status.csv'
+
+CSV.foreach(filepath, csv_options) do |row|
+  puts "#{row['Name']}, a #{row['Appearance']} beer from #{row['Origin']}"
+end
